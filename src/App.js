8d5c8529e4cb03b230d5a69dfdef38b3195ba029
jnoony <tarot0312@nate.com>
2021-11-25 18:22:03 +0900

    포트폴리오 데모사이트 허브페이지 기반

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
+
+import Mobintouch from '/mobintouch/src/index.js'
+
+
+let originURL = window.location.origin;
+
+function App() { 
+  return (
+    <div className="App">
+      <div>ㅠㅠㅠ</div>
+      <a href={`${originURL}/webfertile/`} className="btn-style waves-effect waves-red" target="_blank">webfertile</a>
+      <a href={`${originURL}/promotion/game/src/`} className="btn-style waves-effect waves-red" target="_blank">promotion</a>
+      <a href={`${originURL}/maxsummit/`} className="btn-style waves-effect waves-red" target="_blank">maxsummit 2019</a>
+      <a href={`${originURL}/mtc1st/`} className="btn-style waves-effect waves-red" target="_blank">MaxTheCreative 2019 1st</a>
+      <a href={`${originURL}/mobintouch/`} className="btn-style waves-effect waves-red" target="_blank">mobintouch</a>
+      <Router>
+        <Switch>
+          <Route path="/mobintouch" exact component={Mobintouch}>[DEMO] 모비인터치</Route>
+          {/* <Route path="/mtc2nd" exact component={MTC1st}>[DEMO] 맥스더크리에이티브 2nd</Route> */}
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mobintouch/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router-dom';
+import Root from './client/Root';
+
+import './index.css';
+
+// 기본 webpack이 설정되어 있는 create-react-app 이 프로젝트 베이스.
+// webpack 설정 커스텀 하고 싶다면 npm run eject 해서 처음부터 설정해야함. [※npm run eject 한 후엔 되돌릴수 없음] 
+// node_modules / react-scripts / config 에서 webpack 구성을 찾을수 있음.
+// shared/App.js 파일이 router 기능.
+// 차후 리덕스 사용을 고려하여 client/Root.js 로 연결
+
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
